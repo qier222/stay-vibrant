68135fb59a1f1c8bfb4691ffce463cf3a8f6c557
qier222 <qier222@outlook.com>
2021-06-09 17:07:04 +0800

    support splitbee

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import splitbee from "@splitbee/web";
+
+// This initiliazes Splitbee.js
+splitbee.init();
 
 ReactDOM.render(
   <React.StrictMode>
